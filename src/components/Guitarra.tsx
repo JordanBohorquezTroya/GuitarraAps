6b6a345b702e04a73122fa56dfe81e37988bd7c9
Jordan Bohorquez <jordanbohorqueztroya@gmail.com>
2024-10-26 09:27:09 -0500

    Cambios

--- src/components/Guitarra.tsx
+++ src/components/Guitarra.tsx
@@ -15,7 +15,7 @@ export const Guitarra = ({guitar, addToCart} : GuitarProps) => {
       <div className="col-4">
         <img
           className="img-fluid"
-          src= {`/img/${image}.jpg`}
+          src= {`./img/${image}.jpg`}
           alt="imagen guitarra"
         />
       </div>
